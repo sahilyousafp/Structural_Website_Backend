79caf93b6afefc4e6d8e4bee31ffc5f9f62fdca0
Sahil Yousaf <sahil.yousaf@students.iaac.net>
2025-06-19 03:53:04 +0200

    added port 0.0.0.0

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def root():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
